346835f2333feaaf2cb73a4f476cf8722b5d5ca1
adeel <adeelcomsats070@gmail.com>
2024-11-01 15:38:16 +0500

    changes meta data

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,12 @@ import Solution from "./components/home/solution";
 import SuccessFullWork from "./components/home/successFullWork";
 import TechnologySolution from "./components/home/technologySolution";
 
+
+export const metadata = {
+  title: 'The East Trading Company | Maximize Your Forex Trading Potential',
+  description: 'Invest with the World’s Leading Online Trading Platform. Our MetaTrader platform ensures stable performance and competitive spreads, giving you the advantage you need to make the most of every trade.',
+}
+
 export default function Home() {
   const t = useTranslations("HomePage");
 
@@ -13,7 +19,7 @@ export default function Home() {
       <HeroBanner />
       <div className=" bg-white">
         <Service />
-        <div className=" h-2 w-full bg-primary"/>
+        <div className=" h-2 w-full bg-primary" />
         <Solution />
         <div className=" bg-black">
           <TechnologySolution />
