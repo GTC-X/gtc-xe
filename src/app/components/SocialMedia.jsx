d4d913469824acc6664594bae83663b6654c0244
adeel <a.nazeer@consultant.qdsnet.com>
2024-11-20 19:27:32 +0500

    changes social

--- src/app/components/SocialMedia.jsx
+++ src/app/components/SocialMedia.jsx
@@ -1,39 +1,88 @@
-import React from 'react';
+'use client';
+import React, { useState } from "react";
 import {
   FaFacebookF,
-  FaSnapchat ,
-  FaWhatsapp ,
-  FaPhoneAlt ,
+  FaSnapchat,
+  FaWhatsapp,
+  FaPhoneAlt,
   FaYoutube,
   FaDiscord,
   FaTelegram,
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { RiGlobalLine } from "react-icons/ri";
-import Link from 'next/link';
-
-
+import Link from "next/link";
 
 const socialMediaLinks = [
-  { icon: <FaFacebookF />, label: "Facebook", link: "https://www.facebook.com/EastFX.Ly?locale=ar_AR" },
-  { icon: <FaSnapchat />, label: "FaSnapchat", link: "https://www.snapchat.com/add/eastfx.ly?share_id=489ly-zwc4Q&locale=ar-LY" },
-  { icon: <FaWhatsapp  />, label: "FaWhatsapp ", link: "https://wa.me/message/NR6BJPSHIMA6F1" },
-  { icon: <HiOutlineMail />, label: "Instagram", link: "mailto:info@theastrade.com" },
-  { icon: <FaPhoneAlt  />, label: "phone", link: "tel:00218911730700" },
-  { icon: <RiGlobalLine />, label: "Discord", link: "http://eastfx.ly/" },
+  {
+    icon: <FaFacebookF size={18} className="text-secondary" />,
+    bg: "bg-secondary",
+    label: "Facebook",
+    link: "https://www.facebook.com/EastFX.Ly?locale=ar_AR",
+  },
+  {
+    icon: <FaSnapchat size={18} className="text-secondary" />,
+    bg: "bg-secondary",
+    label: "Snapchat",
+    link: "https://www.snapchat.com/add/eastfx.ly?share_id=489ly-zwc4Q&locale=ar-LY",
+  },
+  {
+    icon: <FaWhatsapp size={18} className="text-secondary" />,
+    bg: "bg-secondary",
+    label: "WhatsApp",
+    link: "https://wa.me/message/NR6BJPSHIMA6F1",
+  },
+  {
+    icon: <HiOutlineMail size={18} className="text-secondary" />,
+    bg: "bg-secondary",
+    label: "Email",
+    link: "mailto:info@theastrade.com",
+  },
+  {
+    icon: <FaPhoneAlt size={18} className="text-secondary" />,
+    bg: "bg-secondary",
+    label: "Phone",
+    link: "tel:00218911730700",
+  },
+
 ];
 
 const SocialMedia = () => {
+  const [active, setActive] = useState(null);
+
   return (
-    <div className="flex justify-center gap-2">
+    <div className="flex flex-col items-end fixed top-1/3 right-0 gap-1.5">
       {socialMediaLinks.map((social, index) => (
-        <div key={index} className="p-1 rounded-full border border-white hover:bg-secondary">
+        <div
+          key={index}
+          className={`flex items-center w-fit p-2 rounded-tl-full rounded-bl-full transition-all duration-300 ease-in-out transform ${active === index ? "scale-105 shadow-lg" : "scale-100"
+            } ${social?.bg}`}
+          onMouseEnter={() => setActive(index)}
+          onMouseLeave={() => setActive(null)}
+        >
           <Link
             href={social.link}
             aria-label={social.label}
-            className="text-gray-400 hover:text-white hover:bg-secondary"
+            className="flex items-center text-white"
           >
-            {social.icon}
+            {/* Icon */}
+            <div
+              className={`bg-white p-1.5 rounded-full transition-transform duration-300 ${active === index ? "scale-125" : "scale-100"
+                }`}
+            >
+              {social.icon}
+            </div>
+            {/* Label */}
+            {active === index &&
+              <span
+                className={`ml-3 whitespace-nowrap transition-all duration-300 ease-in-out transform ${active === index
+                  ? "opacity-100 translate-x-0"
+                  : "opacity-0 -translate-x-5"
+                  }`}
+              >
+                {social.label}
+              </span>
+            }
           </Link>
         </div>
       ))}
