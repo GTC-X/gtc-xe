2ec3edac1328e3b6815e7e2c4b6bcc6a38d4da3f
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2024-11-21 10:35:06 +0400

    updated the path

--- src/app/components/topBar.jsx
+++ src/app/components/topBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Language from './Language';
 import { useTranslation } from 'react-i18next';
+import Link from 'next/link';
 
 const TopBar = () => {
   const { t } = useTranslation();
@@ -8,15 +9,11 @@ const TopBar = () => {
   return (
     <div className=" container hidden md:flex justify-end pb-2 items-center space-x-4">
       {/* Member Login */}
-      <button className="flex items-center text-xs bg-gray-800 text-white hover:bg-gray-700 py-1 px-3 ">
+      <Link href="https://mygtcportal.com/" className="flex items-center text-xs bg-gray-800 text-white hover:bg-gray-700 py-1 px-3 ">
         <span className="ltr:mr-2 rtl:ml-2">🔑</span> {t("HomePage.topBar.memberLogin")}
-      </button>
+      </Link>
 
-      {/* Open an Account Button */}
-      <button className="text-xs bg-green-600 text-white hover:bg-green-700 py-1 px-3 ">
-       {t('HomePage.topBar.openAccount')}
-      </button>
-
+  
 
       {/* Language Dropdown */}
       <div className=" flex gap-2 items-center">
